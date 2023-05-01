62f1029b2fc23f8760315a2a33b0ef8f88c9c18b
Kazannikov Oleg <mobdiz@yandex.ru>
2023-05-01 03:40:44 +0300

    Update public path

--- triangles/vue/vite.config.ts
+++ triangles/vue/vite.config.ts
@@ -3,9 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: isProduction ? '/triangles/vue/dist/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
